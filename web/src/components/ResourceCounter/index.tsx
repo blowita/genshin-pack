4cdd6b19c2a8f775e551b7f7990a790a56cac729
BlowITA <brblowgames@gmail.com>
2020-11-26 21:39:31 -0300

    ResouceCounter styling

--- web/src/components/ResourceCounter/index.tsx
+++ web/src/components/ResourceCounter/index.tsx
@@ -1,30 +1,73 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
+import { FaStar } from 'react-icons/fa'
 import { TiMinus, TiPlus } from 'react-icons/ti'
 
-import { Buttons, Container, Counter } from './styles'
+import { Buttons, Container, Counter, Filler, Stars } from './styles'
 
-const requirement = 144
+const rarity = 5
+const name = `Boreal Wolf's Cracked Tooth (weapon ascension material - rarity ${rarity})`
+const requirement = 0
+
+const integerRegexp = /^[0-9]*$/
 
 const Resources: React.FC = () => {
   const [counter, setCounter] = useState(0)
+  const [hideButtons, setHideButtons] = useState(true)
 
-  const increment = useCallback(() => setCounter((c) => c + 1), [])
+  const increment = useCallback(
+    () => setCounter((c) => Math.min(c + 1, 9999)),
+    []
+  )
+
   const decrement = useCallback(() => setCounter((c) => Math.max(c - 1, 0)), [])
 
+  const changeCounter = useCallback((e) => {
+    if (integerRegexp.test(e.target.value)) {
+      const input = Number(e.target.value)
+      if (input >= 0 && input < 10000) {
+        setCounter(input)
+      }
+    }
+  }, [])
+
+  const fulfilled = useMemo(() => {
+    return !requirement || counter > requirement
+  }, [counter])
+
   return (
-    <Container>
-      <Buttons>
-        <button onClick={decrement}>
+    <Container
+      className={'rarity-' + rarity}
+      onMouseEnter={() => setHideButtons(false)}
+      onMouseLeave={() => setHideButtons(true)}
+    >
+      <Buttons className={hideButtons ? 'hidden' : ''}>
+        <button className="minus" onClick={decrement}>
           <TiMinus />
         </button>
-        <button onClick={increment}>
+        <button className="plus" onClick={increment}>
           <TiPlus />
         </button>
       </Buttons>
+      <Filler />
+      <Stars>
+        {[...Array(rarity).keys()].map((key) => (
+          <FaStar key={key} />
+        ))}
+      </Stars>
       <Counter>
-        <span>
-          {counter}/{requirement}
+        <input
+          aria-label={`${name} stored amount`}
+          value={counter}
+          onChange={changeCounter}
+          size={3}
+        />
+        <span>/</span>
+        <span
+          className={fulfilled ? 'fulfilled' : 'required'}
+          aria-label={`${name} required amount`}
+        >
+          {requirement || '-'}
         </span>
       </Counter>
     </Container>
--- web/src/components/ResourceCounter/styles.ts
+++ web/src/components/ResourceCounter/styles.ts
@@ -10,6 +10,10 @@ export const Buttons = styled.div`
 
   width: 100%;
 
+  &.hidden {
+    visibility: hidden;
+  }
+
   button {
     height: 1.5rem;
     width: 1.5rem;
@@ -21,7 +25,7 @@ export const Buttons = styled.div`
     transition: background-color 0.2s;
   }
 
-  button:first-child {
+  button.minus {
     background: ${shade(0.2, '#ff3333')};
 
     &:hover {
@@ -29,7 +33,7 @@ export const Buttons = styled.div`
     }
   }
 
-  button:last-child {
+  button.plus {
     background: ${shade(0.2, '#33ff33')};
 
     &:hover {
@@ -48,24 +52,71 @@ export const Container = styled.div`
   height: 6rem;
   border-radius: 0.3rem;
 
-  background: #999999
-    url('https://static.wikia.nocookie.net/gensin-impact/images/7/71/Item_Boreal_Wolf%27s_Cracked_Tooth.png')
+  background: url('https://static.wikia.nocookie.net/gensin-impact/images/7/71/Item_Boreal_Wolf%27s_Cracked_Tooth.png')
     no-repeat top center;
   background-size: contain;
+  background-color: #aaaaaa;
+
+  &.rarity-2 {
+    background-color: #6faa88;
+  }
+  &.rarity-3 {
+    background-color: #6faacc;
+  }
+  &.rarity-4 {
+    background-color: #886faa;
+  }
+  &.rarity-5 {
+    background-color: #cc9000;
+  }
 `
 
 export const Counter = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: flex-end;
 
   width: 100%;
   border-radius: 0 0 0.3rem 0.3rem;
 
-  background: #666666;
+  background: white;
+  padding-right: 0.2rem;
 
-  /*  use when requirement is greater than 9999
+  input {
+    color: black;
+    font-weight: 500;
+    border: none;
+    background-color: transparent;
+    text-align: right;
+  }
+
   span {
-    font-size: 0.8rem;
-  } */
+    color: black;
+    font-weight: 500;
+    cursor: default;
+
+    &.required {
+      color: red;
+    }
+
+    &.fulfilled {
+      color: green;
+    }
+  }
+`
+
+export const Filler = styled.div`
+  flex-grow: 1;
+`
+
+export const Stars = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  width: 100%;
+  margin-bottom: 0.2rem;
+
+  color: #ffcc00;
+  font-size: 0.9rem;
 `
