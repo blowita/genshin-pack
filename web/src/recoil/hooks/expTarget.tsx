1018c7624ded9b5eeb750508e8cf17cfd4a43e86
BlowITA <brblowgames@gmail.com>
2021-12-11 01:11:02 -0300

    Improved exp target functionality.

--- web/src/recoil/hooks/expTarget.tsx
+++ web/src/recoil/hooks/expTarget.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-import { CharacterEntity, characterStore } from "../entities";
+import { CharacterEntity, characterStore, resourceStore } from "../entities";
 
 import { characterIds } from "../../data/characters";
 import { expTable } from "../../data/levelingData";
@@ -14,6 +12,15 @@ const calculateExpForCharacter = (character: CharacterEntity): number => {
 
 const useExpTarget = (rarity: number): { expBooks: number } => {
   const characters = characterStore.useEntityList(characterIds);
+  const rarity4XpBook = resourceStore.useEntityValue(
+    "980840b2-b68a-4f5b-8ec8-ea1a7feb07fb"
+  );
+  const rarity3XpBook = resourceStore.useEntityValue(
+    "6efcc643-6ba1-46d8-b8e3-bc534ce868f1"
+  );
+  const rarity2XpBook = resourceStore.useEntityValue(
+    "3bf43090-9ee8-4b45-825f-c3b19b420f95"
+  );
 
   const calculateExpTarget = (): number => {
     return characters.reduce((sum, character) => {
@@ -22,20 +29,43 @@ const useExpTarget = (rarity: number): { expBooks: number } => {
     }, 0);
   };
 
-  let expTarget = calculateExpTarget();
+  const expTargetScaledDown = Math.ceil(calculateExpTarget() / 1000);
 
-  // get ExpBook numbers for each rarity, get their total Exp value, subtract from expTarget, then return appropriate value for indicated rarity
-  expTarget = expTarget - 0;
+  if (!expTargetScaledDown) {
+    return { expBooks: 0 };
+  }
+
+  const usableStock4 = Math.min(
+    rarity4XpBook.stock,
+    Math.floor(expTargetScaledDown / 20)
+  );
+  const usableStock3 = Math.min(
+    rarity3XpBook.stock,
+    Math.floor((expTargetScaledDown - usableStock4 * 20) / 5)
+  );
+  const usableStock2 = Math.min(
+    rarity2XpBook.stock,
+    expTargetScaledDown - usableStock4 * 20 - usableStock3 * 5
+  );
+
+  let mustFit =
+    expTargetScaledDown - usableStock4 * 20 - usableStock3 * 5 - usableStock2;
+
+  const target4 = usableStock4 + Math.floor(mustFit / 20);
+  mustFit -= 20 * Math.floor(mustFit / 20);
+
+  const target3 = usableStock3 + Math.floor(mustFit / 5);
+  mustFit -= 5 * Math.floor(mustFit / 5);
 
   switch (rarity) {
     case 4:
-      return { expBooks: Math.ceil(expTarget / 20000) };
+      return { expBooks: target4 };
     case 3:
-      return { expBooks: Math.ceil((expTarget % 20000) / 5000) };
+      return { expBooks: target3 };
     case 2:
-      return { expBooks: Math.ceil(((expTarget % 20000) % 5000) / 1000) };
+      return { expBooks: usableStock2 + mustFit };
     default:
-      return { expBooks: Math.ceil(expTarget / 20000) };
+      return { expBooks: 0 };
   }
 };
 
